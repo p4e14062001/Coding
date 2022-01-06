002adf177b27019ff959cbc0a17726738c840a9d
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-06 17:35:35 +0530

    commit

--- Coding.py
+++ Coding.py
@@ -16,3 +16,25 @@ class Hash_Sort:
                 equal.append(i)
         equal = sorted(equal)
         return [equal[0], str(M)]
+
+class Dynamic_Programming:
+    '''
+    https://www.interviewbit.com/problems/0-1-knapsack/
+    '''
+    def knapsack_01(self, values, weights, capacity):
+        L = len(values)
+        dp = [[0 for _ in range(capacity + 1)] for _ in range(L + 1)]
+        for i in range(L + 1):
+            for j in range(capacity + 1):
+                if i == 0 or j == 0:
+                    dp[i][j] = 0
+                elif weights[i] <= j:
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i - 1])
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        return dp[L][capacity]
+    '''
+    https://leetcode.com/problems/partition-equal-subset-sum/
+    '''
+    def partition_equal_sum(self, arr):
+        
