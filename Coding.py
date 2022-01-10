d194edccb7eb911660c193263108425b2578fba0
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-10 14:35:18 +0530

    commit

--- Coding.py
+++ Coding.py
@@ -53,3 +53,21 @@ class Dynamic Programming:
         if ans == float('inf'):
             return -1
         return ans
+    '''
+    https://leetcode.com/problems/coin-change/ ---> MEDIUM
+    '''
+    def solve(self, amount, coins, values):
+        for i in range(len(values)):
+            for j in range(len(values[0])):
+                if j == 0:
+                    values[i][j] = 0
+                elif coins[i - 1] <= j:
+                    values[i][j] = min(values[i - 1][j], values[i][j - coins[i - 1]] + 1)
+                else:
+                    values[i][j] = values[i - 1][j]
+        return values[-1][-1]
+    def coinChangeTabulation(self, coins, amount):
+        ans = self.solve(amount, coins, [[float('inf') for _ in range(amount + 1)] for _ in range(len(coins) + 1)])
+        if ans == float('inf'):
+            return -1
+        return ans
