d959bed0f3e71a757b85563c0d675a74bb607956
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-13 14:33:34 +0530

    commit

---
+++ Geometry.py
@@ -0,0 +1,17 @@
+'''
+Links:
+1. Geeks for Geeks: https://practice.geeksforgeeks.org/explore/?category%5B%5D=Geometric&page=1&category%5B%5D=Geometric
+2. Lintcode: https://www.lintcode.com/problem-tag/434/
+3. CSES: https://cses.fi/dt/task/315
+4. Leetcode: https://leetcode.com/problemset/all/?topicSlugs=geometry
+'''
+class Geometry:
+    '''
+    https://cses.fi/dt/task/315
+    '''
+    def volume(self, r):
+        import math
+        return (math.pi) * (4 / 3) * (r ** 3)
+    '''
+    '''
+    def 
