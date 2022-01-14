b7ad97632b74b276d15af4e5371823d1d6993779
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-14 11:10:23 +0530

    commit

--- CSES.py
+++ CSES.py
@@ -1,87 +1,250 @@
-'''
-Links:
-1. Geeks for Geeks: https://practice.geeksforgeeks.org/explore/?category%5B%5D=Geometric&page=1&category%5B%5D=Geometric
-2. Lintcode: https://www.lintcode.com/problem-tag/434/
-3. CSES: https://cses.fi/dt/task/315
-4. Leetcode: https://leetcode.com/problemset/all/?topicSlugs=geometry
-'''
-class Geometry:
+class CSES:
     '''
-    https://cses.fi/dt/task/315
+    https://cses.fi/dt/task/312
+    https://practice.geeksforgeeks.org/problems/addition-of-two-numbers0812/1
     '''
-    def volume(self, r):
-        import math
-        return (math.pi) * (4 / 3) * (r ** 3)
+    def amount(self, a, b):
+        return a + b
     '''
-    https://practice.geeksforgeeks.org/problems/line-passing-through-2-points5031/1
+    https://cses.fi/dt/task/313
     '''
-    def getLine(self, a, b, c, d):
-        x1 = a
-        x2 = c
-        y1 = b
-        y2 = d
-        c1 = (y2 - y1)
-        c2 = (x1 - x2)
-        c = x1 * (y2 - y1) - y1 * (x2 - x1)
-        if c2 >= 0:
-            return (str(c1) + "x+" + str(abs(c2)) + "y=" + str(c))
+    def algorithm(self, a):
+        while a != 1:
+            print(a, end = ' ')
+            if a % 2 == 0:
+                a = a // 2
+            else:
+                a = 3 * a + 1
+        print(1)
+    '''
+    https://cses.fi/dt/task/314
+    '''
+    def bit_strings(self, n):
+        return 2 ** n
+    '''
+    https://cses.fi/dt/task/316
+    https://practice.geeksforgeeks.org/problems/day-of-the-week1637/1/
+    https://leetcode.com/problems/day-of-the-week/
+    https://www.lintcode.com/problem/2661/
+    '''
+    def day_from_date(self, s):
+        s = s.split('.')
+        day = int(s[0])
+        month = int(s[1])
+        year = int(s[2])
+        k = day
+        m = month - 2
+        if m <= 0:
+            m = m + 12
+        C = year // 100
+        Y = year % 100
+        if month == 1 or month == 2:
+            Y = Y - 1
+        W = (k + int(2.6 * m - 0.2) - 2 * C + Y + Y // 4 + C // 4) % 7
+        days = ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"]
+        return days[W]
+    '''
+    https://cses.fi/dt/task/317/
+    '''
+    def max_repeat(self, s):
+        s = input()
+        m = float('-inf')
+        c = 1
+        for i in range(1, len(s)):
+            if s[i - 1] == s[i]:
+                c = c + 1
+            else:
+                m = max(m, c)
+                c = 1
+        return max(m, c)
+    '''
+    https://cses.fi/dt/task/318/
+    '''
+    def miss(self, n, s):
+        S = 0
+        for i in range(n - 1):
+            S = S + int(s[i])
+        return (n * (n + 1) // 2 - S)
+    '''
+    https://cses.fi/dt/task/319/
+    https://cses.fi/problemset/task/1071/
+    '''
+    def spiral(self, c):
+        c = c.split()
+        x = int(c[0])
+        y = int(c[1])
+        ans = 0
+        if y > x:
+            if y % 2 != 0:
+                ans = y * y - x + 1
+            else:
+                ans = (y - 1) * (y - 1) + x
         else:
-            return (str(c1) + "x-" + str(abs(c2)) + "y=" + str(c))
+            if x % 2 == 0:
+                ans = x * x - y + 1
+            else:
+                ans = (x - 1) * (x - 1) + y
+        return ans
     '''
-    https://practice.geeksforgeeks.org/problems/number-of-diagonals1020/1
+    https://cses.fi/dt/task/320
     '''
-    def diagonals(self, n):
-        if n % 2 == 0:
-            return (n // 2) * (n - 3)
-        else:
-            return ((n - 3) // 2) * n
+    def knight(self, n):
+        return (n * n * (n * n - 1) // 2) - 4 * (n - 1) * (n - 2)
     '''
-    https://practice.geeksforgeeks.org/problems/find-perimeter-of-shapes/1
+    https://cses.fi/dt/task/321/
     '''
-    def findPerimeter(arr, n, m):
-        x = 0
+    def zeros(self, n):
+        z = 0
+        while n != 0:
+            n = n // 5
+            z = z + n
+        return z
+    '''
+    https://cses.fi/dt/task/322/
+    '''
+    def customers(self, s):
+        a = []
+        d = []
+        while t:
+            s = s.split()
+            a.append(int(s[0]))
+            d.append(int(s[1]))
+            t = t - 1
+        checka = {}
+        checkd = {}
+        for i in a:
+            if i not in checka:
+                checka[i] = 0
+            checka[i] = checka[i] + 1
+        for i in d:
+            if i not in checkd:
+                checkd[i] = 0
+            checkd[i] = checkd[i] - 1
+        timeline = sorted(list(checka.items()) + list(checkd.items()))
+        m = float('-inf')
+        c = 0
+        for i in timeline:
+            c = c + i[1]
+            m = max(m, c)
+        return m
+    '''
+    https://cses.fi/dt/task/323/
+    '''
+    def deals(self, s, w, a):
+        s = s.split()
+        n = int(s[0])
+        m = int(s[1])
+        k = int(s[2])
+        w = w.split()
         for i in range(n):
-            flag = 0
-            for j in range(m):
-                if flag == 0 and arr[i][j] == 1:
-                    flag = 1
-                    x = x + 1
-                if flag == 1 and arr[i][j] == 0:
-                    flag = 0
-        for j in range(m):
-            flag = 0
-            for i in range(n):
-                if flag == 0 and arr[i][j] == 1:
-                    flag = 1
-                    x = x + 1
-                if flag == 1 and arr[i][j] == 0:
-                    flag = 0
-        return x * 2
+            w[i] = int(w[i])
+        a = a.split()
+        for i in range(m):
+            a[i] = int(a[i])
+        w.sort()
+        a.sort()
+        wishi = 0
+        geti = 0
+        deal = 0
+        while wishi != n and geti != m:
+            if w[wishi] - a[geti] > k:
+                geti = geti + 1
+            elif abs(w[wishi] - a[geti]) <= k:
+                geti = geti + 1
+                wishi = wishi + 1
+                deal = deal + 1
+            elif w[wishi] - a[geti] < - k:
+                wishi = wishi + 1
+        return deal
     '''
-    https://practice.geeksforgeeks.org/problems/distance-between-2-points3200/1
+    https://cses.fi/dt/task/324
     '''
-    def distance(self, x1, y1, x2, y2):
-		return round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5)
-    '''
-    https://practice.geeksforgeeks.org/problems/circle-and-lattice-points4257/1
-    '''
-    def latticePoints(self, r):
-        if r == 0:
-            return 0
-        x = 0
-        for i in range(r + 1):
-            if int((r ** 2 - i ** 2) ** 0.5) ** 2 + i ** 2 == r ** 2:
-                if i == 0 or r * r - i * i == 0:
-                    x = x + 2
+    def ferris(self, s, w):
+        s = s.split()
+        n = int(s[0])
+        M = int(s[1])
+        w = w.split()
+        for i in range(n):
+            w[i] = int(w[i])
+        w.sort()
+        start = 0
+        end = n - 1
+        baskets = 0
+        while start <= end:
+            baskets = baskets + 1
+            if start != end:
+                if w[start] + w[end] > M:
+                    end = end - 1
                 else:
-                    x = x + 4
-        return x
+                    start = start + 1
+                    end = end - 1
+            else:
+                break
+        return baskets
     '''
-    https://practice.geeksforgeeks.org/problems/checcheck-if-two-given-circles-touch-each-other5038/1
-    https://www.spoj.com/problems/CIRCINT/
+    https://cses.fi/dt/task/325
     '''
-    def circleTouch(self,X1,Y1,R1,X2,Y2,R2):
-        return int(((X2 - X1) ** 2 + (Y2  - Y1) ** 2) ** 0.5 <= (R1 + R2))
+    def factory(self, s, t):
+        s = s.split()
+        n = int(s[0])
+        p = int(s[1])
+        t = t.split()
+        for i in range(n):
+            t[i] = int(t[i])
+        m = max(t)
+        left = 0
+        right = m * p + 1
+        ans = 0
+        current_p = 0
+        while left <= right:
+            mid = left + (right - left) // 2
+            current_p = 0
+            for i in range(n):
+                current_p = current_p + mid // t[i]
+            if current_p >= p:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
     '''
-    https://practice.geeksforgeeks.org/problems/rectangles-in-a-circle0457/1
+    https://cses.fi/dt/task/609/
     '''
+    def order(self, n, s):
+        n = int(n)
+        s = s.split()
+        for i in range(n):
+            s[i] = int(s[i])
+        expected = n
+        for i in range(n - 1, -1, -1):
+            if s[i] == expected:
+                expected = expected - 1
+        return expected
+    '''
+    https://cses.fi/dt/task/326
+    '''
+    def partition(self, S, s):
+        S = S.split()
+        n = int(S[0])
+        k = int(S[1])
+        s = s.split()
+        for i in range(n):
+            s[i] = int(s[i])
+        left = max(s)
+        right = sum(s)
+        ans = 0
+        while left <= right:
+            mid = int(left + (right - left) / 2)
+            sa = 1
+            sums = 0
+            for i in range(n):
+                sums = sums + s[i]
+                if sums > mid:
+                    sa = sa + 1
+                    sums = s[i]
+            if sa <= k:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
