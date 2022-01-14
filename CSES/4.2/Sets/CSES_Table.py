4461c9fa5131386e1c3e89adb9ab9d7f2619b834
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-14 23:29:16 +0530

    commit

---
+++ CSES/4.2/Sets/CSES_Table.py
@@ -0,0 +1,56 @@
+'''
+1. https://cses.fi/dt/task/364/
+'''
+class Set_4_2:
+    def __init__(self):
+        self.sets = {}
+    def print_set(self):
+        print(self.sets)
+    def length(self):
+        return len(self.sets)
+    def anything_to_set(self, l):
+        self.sets = set(l)
+    def check(self, element):
+        return element in self.sets
+    def addition(self, element):
+        self.sets.add(element)
+    def union_sets(self, setb):
+        self.sets.update(setb)
+    def intersection_sets(self, setb):
+        self.sets.intersection_update(setb)
+    def difference_sets(self, setb):
+        self.sets.symmetric_difference_update(setb)
+    def remove(self, element):
+        if element in self.sets:
+            self.sets.remove(element)
+    def discard(self, element):
+        self.sets.discard(element)
+    def see_and_remove_last(self, element):
+        x = self.sets.pop()
+        return x
+    def empty_set(self):
+        self.sets.clear()
+    def delete_set(self):
+        del self.sets
+    def union(self, setb):
+        return self.sets.union(setb)
+    def intersection(self, setb):
+        return self.sets.intersection(setb)
+    def difference(self, setb):
+        return self.sets.symmetric_difference(setb)
+    def duplicate(self):
+        return self.sets.copy()
+    def disjoint(self, setb):
+        return self.sets.isdisjoint(setb)
+    def subset(self, setb):
+        return self.sets.issubset(setb)
+    def superset(self, setb):
+        return self.sets.issuperset(setb)
+
+n = int(input())
+s = input().split()
+for i in range(n):
+    s[i] = int(s[i])
+S = Set_4_2()
+S.anything_to_set(s)
+print(S.length())
