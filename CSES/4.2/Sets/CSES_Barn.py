baa2c1ac35746e3319d312fb9827b58fb2c4d1fc
p4e14062001 <97163727+p4e14062001@users.noreply.github.com>
2022-01-15 09:02:50 +0530

    commit

---
+++ CSES/4.2/Sets/CSES_Barn.py
@@ -0,0 +1,67 @@
+'''
+1. https://cses.fi/dt/task/328/
+'''
+class Set_4_2:
+    def __init__(self):
+        self.sets = set()
+    def print_set(self):
+        print(self.sets)
+    def length(self):
+        return len(self.sets)
+    def anything_to_set(self, l):
+        self.sets = set(l)
+    def check(self, element):
+        return element in self.sets
+    def addition(self, element):
+        self.sets.add(element)
+    def union_sets(self, setb):
+        self.sets.update(setb)
+    def intersection_sets(self, setb):
+        self.sets.intersection_update(setb)
+    def difference_sets(self, setb):
+        self.sets.symmetric_difference_update(setb)
+    def remove(self, element):
+        if element in self.sets:
+            self.sets.remove(element)
+    def discard(self, element):
+        self.sets.discard(element)
+    def see_and_remove_last(self, element):
+        x = self.sets.pop()
+        return x
+    def empty_set(self):
+        self.sets.clear()
+    def delete_set(self):
+        del self.sets
+    def union(self, setb):
+        return self.sets.union(setb)
+    def intersection(self, setb):
+        return self.sets.intersection(setb)
+    def difference(self, setb):
+        return self.sets.symmetric_difference(setb)
+    def duplicate(self):
+        return self.sets.copy()
+    def disjoint(self, setb):
+        return self.sets.isdisjoint(setb)
+    def subset(self, setb):
+        return self.sets.issubset(setb)
+    def superset(self, setb):
+        return self.sets.issuperset(setb)
+
+s = input().split()
+n = int(s[0])
+m = int(s[1])
+S = Set_4_2()
+while m:
+    s = input().split()
+    o = s[0]
+    k = int(s[1])
+    if o == '+':
+        S.addition(k)
+    elif o == '?':
+        if S.check(k):
+            print('10-4')
+        else:
+            print('QAQ')
+    else:
+        S.discard(k)
+    m = m - 1
